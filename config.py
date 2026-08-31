7c1d7ea9ffeda1569de91a97dd329550ccf8d540
Mari-Vasquez <mvasquez@puce.edu.ec>
2026-08-31 11:45:39 -0500

    Modificación URI
    Se coloco el mismo URI para que las configuraciones no se guarde en diferentes archivos.

--- config.py
+++ config.py
@@ -10,8 +10,11 @@ class Config:
     DB_PORT = os.getenv("DB_PORT", "5432")
     DB_NAME = os.getenv("DB_NAME", "tienda_online")
 
+    # Se mantiene la URI en un solo lugar para evitar
+    # configuraciones diferentes entre archivos.
     SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@"
+        f"{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
